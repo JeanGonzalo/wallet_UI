2d6448bef2015ae5f6acafd0b999f8e531e8cc22
jean <rjeangonzalo@gmail.com>
2020-04-09 22:37:45 -0500

    [create] icons send, request, lean, topup

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -12,7 +12,10 @@ class HomeScreen extends StatelessWidget {
           Container(
             margin: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment
+                  .start, // alineacion de eje transversal, puso de lado izquiero la app el TEXT
               children: <Widget>[
+                // todo esto alamcena la foto, campana y el monto
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -46,10 +49,140 @@ class HomeScreen extends StatelessWidget {
                       ),
                     )
                   ],
-                )
+                ),
+
+                Text(
+                  "Aviable Balance",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                      color: Colors.blue[100]),
+                ),
+                //espaciado
+                SizedBox(
+                  height: 24,
+                ),
+                //aqui esta los icons que se colocaran en el mid
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(243, 245, 248, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                            child: Icon(
+                              Icons.date_range,
+                              color: Colors.blue[900],
+                              size: 30,
+                            ),
+                            padding: EdgeInsets.all(12),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            "Send",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                                color: Colors.blue[100]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(243, 245, 248, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                            child: Icon(
+                              Icons.public,
+                              color: Colors.blue[900],
+                              size: 30,
+                            ),
+                            padding: EdgeInsets.all(12),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            "Request",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                                color: Colors.blue[100]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(243, 245, 248, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                            child: Icon(
+                              Icons.attach_money,
+                              color: Colors.blue[900],
+                              size: 30,
+                            ),
+                            padding: EdgeInsets.all(12),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            "Loan",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                                color: Colors.blue[100]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(243, 245, 248, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                            child: Icon(
+                              Icons.date_range,
+                              color: Colors.blue[900],
+                              size: 30,
+                            ),
+                            padding: EdgeInsets.all(12),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            "Send",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                                color: Colors.blue[100]),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
